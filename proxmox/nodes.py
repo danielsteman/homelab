78316daf10d02cb04880e68939831e426e2df06d
Daniel Steman <daniel-steman@live.nl>
2024-10-27 21:52:53 +0100

    use 3.13

--- proxmox/nodes.py
+++ proxmox/nodes.py
@@ -1,6 +1,16 @@
-def main():
-    print("Hello from proxmox!")
+import httpx
+
+BASE_URL = "https://cluster.fuck"
+ENDPOINT = "/api2/json/access/ticket"
+
+
+class ProxmoxManager(httpx.AsyncClient):
+    def __init__(self) -> None:
+        pass
+
+    def get_token(self):
+        pass
 
 
 if __name__ == "__main__":
-    main()
+    manager = ProxmoxManager()
