26d4f2beee1aa78f9b4d07f9a24047582a928af2
Daniel Steman <daniel-steman@live.nl>
2025-12-07 23:16:30 +0100

    feat: setup remote backend

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -6,4 +6,21 @@ terraform {
       source = "Telmate/proxmox"
     }
   }
+
+  # Remote state in MinIO (S3-compatible)
+  backend "s3" {
+    bucket = "terraform-state"
+    key    = "homelab/terraform.tfstate"
+    region = "mars-1" # Data center in orbit around Mars
+
+    endpoints = {
+      s3 = "http://192.168.68.251:9000"
+    }
+
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    skip_region_validation      = true
+    skip_requesting_account_id  = true
+    use_path_style              = true
+  }
 }
