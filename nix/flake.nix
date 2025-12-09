40db5503f9614270fc62a1e448c0690a67b436d5
Daniel Steman <daniel-steman@live.nl>
2025-12-09 23:01:30 +0100

    ci: fix

--- nix/flake.nix
+++ nix/flake.nix
@@ -10,8 +10,16 @@
     nixosConfigurations.pi = nixpkgs.lib.nixosSystem {
       system = "aarch64-linux";
       modules = [
+        "${nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64.nix"
         ./configuration.nix
       ];
     };
+
+    # Build with: nix build .#sdImage
+    packages.aarch64-linux.sdImage =
+      self.nixosConfigurations.pi.config.system.build.sdImage;
+
+    packages.aarch64-linux.default =
+      self.nixosConfigurations.pi.config.system.build.sdImage;
   };
 }
